a43003183c07fde710536d44caf21b12f66e66b1
Norman Huth <norman@huth.it>
2022-01-06 12:06:19 +0100

    Update dependencies, activate version hash & remove webpack from js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css');
+mix
+    .setPublicPath('public')
+    .sass('resources/scss/app.scss', 'css')
+    .scripts(['resources/js/app.js'], 'public/js/app.js')
+    .version();
